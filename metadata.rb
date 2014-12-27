8a22a81b4a26a28793d40126cc8ba94e043a1474
Jimmy Yang <gankkank@gmail.com>
2014-12-27 16:29:10 +0800

    init project

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'web-config'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures web-config'
+long_description 'Installs/Configures web-config'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: web-config
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
